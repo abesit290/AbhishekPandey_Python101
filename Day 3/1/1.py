eb5fea75e9f4d2604ec324db058e051ca26ba084
abhi1137 <36633854+abesit290@users.noreply.github.com>
2018-06-14 07:57:33 +0530

    Update 1.py

--- Day 3/1/1.py
+++ Day 3/1/1.py
@@ -1,1 +1,21 @@
+for number in range(100):  
 
+    if number % 3 == 0: 
+
+        print("Fizz")                                         
+
+        continue
+
+    elif number % 5 == 0:     
+
+        print("Buzz")                                         
+
+        continue
+
+    elif number % 15 == 0:         
+
+        print("FizzBuzz")                                     
+
+        continue
+        
+    print(number)
